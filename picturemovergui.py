071dd78f114f8aeb8f0fce2b39998d807303271e
Devin <ddulude95c@gmail.com>
2024-08-11 22:02:32 -0400

    Added renamer

--- picturemovergui.py
+++ picturemovergui.py
@@ -1,14 +1,15 @@
 #Author: Devin Dulude
-#Version: 1.1 (cleaned up code and comments)
+#Version: 1.2 (Added renamer feature)
 #About: Python program created to use specifically with Fastfoto software to move
 # the non-enhanced versions of your scanned photos to their own folder if you chose to keep them.
-# This way you don't have duplicates in the folder when viewing the photos.
+# This way you don't have duplicates in the folder when viewing the photos. Also can batch rename photos.
 
 import tkinter as tk
 import os, sys, shutil
 from tkinter.filedialog import askdirectory
 #tk.Tk().withdraw() # part of the import if you are not using other tkinter functions
 sourcedir = ""
+TITLE = "Originals Photo Sorter"
 originalsPath = ""
 tocopy = []
 debug = False
@@ -25,21 +26,22 @@ def handleDirectory():
 
 # makes sure the path is valid and exists. Also makes sure we arent in a folder called Originals. Then checks for existing Originals
 # folder and creates it if not exists.
-def checkPath():
+def checkPath(checkOrig: bool):
     global originalsPath, sourcedir
     sourcedir = dir.get()
-    while True:
-        if not(os.path.exists(sourcedir)):
-            print("Invalid path entered. Path does not exist")
-            tk.messagebox.showerror(title="Originals Photo Sorter", message="Invalid file path. Path does not exist.")
-            break
 
+    if not(os.path.exists(sourcedir)):
+        print("Invalid path entered. Path does not exist")
+        tk.messagebox.showerror(title="Originals Photo Sorter", message="Invalid file path. Path does not exist.")
+        return False
+
+    if checkOrig:
+        originalsPath = sourcedir + "/Originals"
         if str(sourcedir).endswith((r'/Originals', r'/Originals/')):
             print("Source folder IS 'Originals'. Source folder cannot be an 'Originals' folder")
             tk.messagebox.showerror(title="Originals Photo Sorter", message="Source folder cannot be 'Originals'.")
-            break
-        
-        originalsPath = sourcedir + "/Originals"
+            return False            
+
         if not (os.path.exists(originalsPath)):
             print("Folder 'Originals' not found...creating")
 
@@ -48,34 +50,51 @@ def checkPath():
             except OSError as error:
                 print(error)
                 print("Couldn't create folder 'Originals', no action has been taken.")
-                break
+                return False
+            return True # If we got here, it was successful in creating the folder            
         else:
             if debug:
                 print("Originals folder already exists, continuing")
             return True
+        
+    else:
+        return True
 
 # makes sure to only handle .jpg files and adds originalsPath to a tocopy list.
-def sortPics():
+def sortPics(rename: bool):
     jpgs = []
     global tocopy
+    numChanges = 0
 
     # grab only jpgs
     for file in os.listdir(sourcedir):
         if str(file).lower().endswith('.jpg'):
             jpgs.append(file)
 
-    # go through jpgs and filter out which to move
-    for picture in jpgs:
-        if (str(picture).endswith('_a.jpg')):
-            continue # we never move anything that ends with _a, so we continue 
+    if rename:
+        for picture in jpgs:
+            old_name = str(picture)
+            if whatfield.get() in old_name:
+                new_name = old_name.replace(whatfield.get(), withfield.get())
+                os.rename( (os.path.join(sourcedir, old_name)), (os.path.join(sourcedir, new_name)) )
+                numChanges += 1
 
-        if moveBside.get() == 0 and str(picture).endswith('_b.jpg'): # if not moving B side & ends with _b, dont add to queue and continue
-            continue
+    else:
+        # go through jpgs and filter out which to move
+        for picture in jpgs:
+            if (str(picture).endswith('_a.jpg')):
+                continue # we never move anything that ends with _a, so we continue 
 
-        # finally, add this pic to queue
-        tocopy.append(picture)
-        if debug:
-            print("queueing " + str(picture))
+            if moveBside.get() == 0 and str(picture).endswith('_b.jpg'): # if not moving B side & ends with _b, dont add to queue and continue
+                continue
+
+            # finally, add this pic to queue
+            tocopy.append(picture)
+            numChanges += 1
+            if debug:
+                print("queueing " + str(picture))
+
+    return numChanges
     
 
 def movePics():
@@ -88,51 +107,94 @@ def cleanup():
     global tocopy
     tocopy = []
 
-def main():
-    global tocopy
-    
-    if dir.get() == "":
-        tk.messagebox.showerror(title="Originals Photo Sorter", message="Directory cannot be blank")    
-        return    
-    if not checkPath():
+def startRename():
+    if not checkPath(False):
         # checkPath will throw the error, so we simply return in here.
         return
     
-    sortPics()
-    
-    if len(tocopy) != 0:
-        movePics()
-        infomsg = "Moved " + str(len(tocopy))  + " pictures"
+    if whatfield.get() == "" or withfield.get() == "":
+        tk.messagebox.showerror(title=TITLE, message="Replace fields cant be blank!")
+        return
+      
+    numRenamed = sortPics(True)
+
+    if numRenamed != 0:
+        infomsg = "Renamed " + str(numRenamed)  + " pictures"
         print(infomsg)
-        tk.messagebox.showinfo(title="Originals Photo Sorter", message=infomsg)
+        tk.messagebox.showinfo(title=TITLE, message=infomsg)
+        
+    else:
+        print("No pictures to rename.")
+        tk.messagebox.showinfo(title=TITLE, message="No pictures to rename.")
+
+def startMove():
+    if not checkPath(True):
+        # checkPath will throw the error, so we simply return in here.
+        return
+    
+    count = sortPics(False)
+    
+    if count != 0:
+        movePics()
+        infomsg = "Moved " + str(count)  + " pictures"
+        print(infomsg)
+        tk.messagebox.showinfo(title=TITLE, message=infomsg)
         
     else:
         print("No pictures to move.")
-        tk.messagebox.showinfo(title="Originals Photo Sorter", message="No pictures to move.")
+        tk.messagebox.showinfo(title=TITLE, message="No pictures to move.")
         
     cleanup()
+
+def main(action):
+    if dir.get() == "":
+        tk.messagebox.showerror(title=TITLE, message="Directory cannot be blank")    
+        return
+
+    match action:
+        case 1:
+            startMove()
+        case 2:
+            startRename()
+            
         
 
 #def guimain():
 print("Originals Photo Sorter by Devin Dulude")
 root = tk.Tk()
-root.title("Originals Photo Sorter")
+root.title(TITLE)
 root.geometry("400x200")
 content = tk.Frame(root)
 frame = tk.Frame(content, borderwidth=5, relief=tk.RAISED, width=400, height=200)
+
+
 dirLabel = tk.Label(content, text="Directory of photos")
 dir = tk.Entry(content, width=50)
+whatlabel = tk.Label(content, text="Replace what")
+whatfield = tk.Entry(content, width=40)
+withlabel = tk.Label(content, text="Replace with")
+withfield = tk.Entry(content, width=40)
+
+
 browseBtn = tk.Button(content, text="Browse", command=handleDirectory)
 moveBside = tk.IntVar()
 bSideBtn = tk.Checkbutton(root, text="Move reverse side photos?", variable=moveBside)
-gobtn = tk.Button(content, text="Go", command=main)
+gobtn = tk.Button(content, text="Move", command=lambda: main(1))
+renamebtn = tk.Button(content, text="Rename", command=lambda: main(2))
 
 content.grid(column=0, row=0)
 dirLabel.grid(column=0, row=0)
 dir.grid(column=0, row=1, padx=5)
+whatlabel.grid(column=0, row=3)
+whatfield.grid(column=0, row=4)
+withlabel.grid(column=0, row=5)
+withfield.grid(column=0, row=6)
+
 browseBtn.grid(column=1, row=1)
 bSideBtn.grid(column=0, row=1, sticky='W')
-gobtn.grid(column=1, row=2)
+gobtn.grid(column=1, row=6)
+renamebtn.grid(column=0, row=7)
+
 root.mainloop()
 
 
